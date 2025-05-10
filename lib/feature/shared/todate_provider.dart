6ba32aa8cfac7930e8032d8dc6359ea1d608f5e6
Chieko Natori <chieko.natori@gmail.com>
2025-05-10 14:50:00 +0800

    Change BlocProvider to MultiBolcProvider

--- lib/feature/shared/todate_provider.dart
+++ lib/feature/shared/todate_provider.dart
@@ -4,23 +4,34 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../feature/shared/todate_cubit.dart';
 import '../../domain/models/todate.dart';
 import '../../domain/repository/todate_repo.dart';
+import '../../domain/repository/user_config_repo.dart';
 import 'todate_rootview.dart';
+import 'user_config_cubit.dart';
 import 'app.dart';
 
 class TodateProvider extends StatelessWidget {
 
   final TodateRepo todateRepo;
+  final UserConfigRepo userConfigRepo;
 
   const TodateProvider({
     super.key,
     required this.todateRepo,
+    required this.userConfigRepo,
   });
 
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => TodateCubit(todateRepo),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<TodateCubit>(
+          create: (context) => TodateCubit(todateRepo),
+        ),
+        BlocProvider<UserConfigCubit>(
+          create: (context) => UserConfigCubit(userConfigRepo),
+        ),
+      ],
       child: const MyApp(),
     );
   }
