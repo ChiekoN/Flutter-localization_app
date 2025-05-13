82d588c22e79c568d817706414baebf2291a6eb7
Chieko Natori <chieko.natori@gmail.com>
2025-05-13 11:55:40 +0800

    Remove debug banner

--- lib/feature/shared/app.dart
+++ lib/feature/shared/app.dart
@@ -19,31 +19,31 @@ class MyApp extends StatelessWidget {
     return BlocBuilder<UserConfigCubit, UserConfig> (
       builder: (context, configState) {
         return MaterialApp.router(
-            title: 'Flutter Demo',
-            //locale: Locale('en'), // TODO: Switch here to change locale
-            locale: configState.locale,
-            localizationsDelegates: AppLocalizations.localizationsDelegates + [LocaleNamesLocalizationsDelegate()],
-            supportedLocales: AppLocalizations.supportedLocales,
-            theme: ThemeData(
-              // This is the theme of your application.
-              //
-              // TRY THIS: Try running your application with "flutter run". You'll see
-              // the application has a purple toolbar. Then, without quitting the app,
-              // try changing the seedColor in the colorScheme below to Colors.green
-              // and then invoke "hot reload" (save your changes or press the "hot
-              // reload" button in a Flutter-supported IDE, or press "r" if you used
-              // the command line to start the app).
-              //
-              // Notice that the counter didn't reset back to zero; the application
-              // state is not lost during the reload. To reset the state, use hot
-              // restart instead.
-              //
-              // This works for code too, not just values: Most code changes can be
-              // tested with just a hot reload.
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            ),
-            //home: const MyHomePage(title: 'Flutter Demo Home Page'),
-            routerConfig: appRouter,
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          locale: configState.locale,
+          localizationsDelegates: AppLocalizations.localizationsDelegates + [LocaleNamesLocalizationsDelegate()],
+          supportedLocales: AppLocalizations.supportedLocales,
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // TRY THIS: Try running your application with "flutter run". You'll see
+            // the application has a purple toolbar. Then, without quitting the app,
+            // try changing the seedColor in the colorScheme below to Colors.green
+            // and then invoke "hot reload" (save your changes or press the "hot
+            // reload" button in a Flutter-supported IDE, or press "r" if you used
+            // the command line to start the app).
+            //
+            // Notice that the counter didn't reset back to zero; the application
+            // state is not lost during the reload. To reset the state, use hot
+            // restart instead.
+            //
+            // This works for code too, not just values: Most code changes can be
+            // tested with just a hot reload.
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          ),
+          //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          routerConfig: appRouter,
         );
       }
     );
